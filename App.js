3f15b22a9795ad60a3d81b8a19cf94325240a6ca
JurisBaltacs <45820251+JurisBaltacs@users.noreply.github.com>
2022-11-10 10:45:59 +0200

    Search without debounce.

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  Text,
+  View,
+  Image,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import { Searchbar } from "react-native-paper";
+import debounce from "lodash/debounce";
 
-export default function App() {
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("chili");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  useEffect(() => {
+    fetch(
+      `http://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=p7D0xFESzcNBPN6fTM595XvT2PUiJ8YV&limit=30`
+    ) // #TODO: ielikt KEY in .env file
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, [searchQuery]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View style={styles.topContainer}>
+          <Text style={styles.title}>GIFs:</Text>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+
+          <FlatList
+            data={data.data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Image
+                source={{
+                  uri: item.images.original.url,
+                }}
+                style={styles.gifContainer}
+              />
+            )}
+          />
+        </View>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  topContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 14,
+    color: "green",
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  searchBar: {
+    height: 50,
+    borderWidth: 1,
+    borderColor: "green",
+  },
+  gifContainer: {
+    width: 100,
+    height: 100,
   },
 });
+
+export default App;
