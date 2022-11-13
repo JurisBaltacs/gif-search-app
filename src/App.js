5f98c9485c36bb4b7288ce2263fc3094a17f6ad9
JurisBaltacs <45820251+JurisBaltacs@users.noreply.github.com>
2022-11-13 22:27:18 +0200

    Calls api on every change.

--- src/App.js
+++ src/App.js
@@ -9,65 +9,68 @@ import {
 } from "react-native";
 import { Searchbar } from "react-native-paper";
 import debounce from "lodash/debounce";
+import PRIVATE_KEY from "./Constants";
 
 const App = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
-  const [isLoadingMoreData, setIsLoadingMoreData] = useState(false);
-  const [itemLimit, setItemLimit] = useState(10);
+  // const [isLoadingMoreData, setIsLoadingMoreData] = useState(false);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    setData([]);
+  }, [query]);
 
   const WINDOW_PADDING = 10;
   const windowWidth = useWindowDimensions().width;
   const gifSize = (windowWidth - WINDOW_PADDING * 2) / 3;
-
-  // const windowWidth = useWindowDimensions().width;
-  // console.log(windowWidth);
-
-  useEffect(() => {
-    handleSearch();
-  }, []);
+  const itemLimit = 20;
 
   const handleChange = (input) => {
+    // if (!input) {
+    //   return;
+    // } else {
     setQuery(input);
-    handleSearch(input);
+    // handleSearch(input);
+    // console.log("input", input);
+    // console.log("query", query);
+    // }
   };
 
-  // #TODO: ielikt KEY in .env file
-  const handleSearch = useCallback(
-    debounce((value = "") => {
-      fetch(
-        `http://api.giphy.com/v1/gifs/search?q=${value}&api_key=p7D0xFESzcNBPN6fTM595XvT2PUiJ8YV&limit=${itemLimit}`,
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
-      )
-        .then((response) => response.json())
-        // .then((json) => setData(json))
-        .then((json) => {
-          console.log("json?.data", json?.data);
-          console.log("data: ", data);
-          setData([...data, ...json?.data]);
-        })
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    }, 300),
-    []
-  );
+  // #TODO: Nepushot constants failu
+  const handleSearch = debounce((input = "space") => {
+    console.log("input of handleSearch: ", input);
 
-  const handleLoadMore = () => {
-    setIsLoadingMoreData(true);
+    fetch(
+      `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=${PRIVATE_KEY}&limit=${itemLimit}&offset=${offset}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      // .then((json) => setData(json))
+      .then((json) => {
+        if (json?.data.length > 0) setOffset(offset + 20);
+        setData([...data, ...json?.data]);
+        // console.log("json =>", json);
+        // setData([...json?.data]);
+      })
 
-    // setPage(() => page + 1);
-    // () => fetchMarketData;
-  };
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, 500);
 
-  // console.log(data);
-  console.log("itemLimit", itemLimit);
+  useEffect(() => {
+    handleSearch(query, [query]);
+    console.log("run useEffect");
+  }, [query]);
+
+  // console.log("data end=>", data);
   return (
     <View style={{ flex: 1, paddingHorizontal: WINDOW_PADDING }}>
       {isLoading ? (
@@ -80,6 +83,7 @@ const App = () => {
             placeholder="Type your search here"
             onChangeText={handleChange}
             value={query}
+            // value={input}
             style={styles.searchBar}
 
             // #TODO: ieviest onSend. Tagad nekas nenotiek.
@@ -89,9 +93,9 @@ const App = () => {
             data={data}
             keyExtractor={(item) => item.id}
             numColumns={3}
-            onEndReachedThreshold={0.01}
-            // onEndReached={() => handleLoadMore()}
-            onEndReached={() => setItemLimit(itemLimit + 10)}
+            onEndReachedThreshold={0.3}
+            // onEndReached={() => setItemLimit(itemLimit + 20)}
+            onEndReached={() => handleSearch(query)}
             renderItem={({ item }) => (
               <Image
                 source={{
