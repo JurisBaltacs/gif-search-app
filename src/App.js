3123ce85bc37ad834c59be923f8e5f19be01a2f4
JurisBaltacs <45820251+JurisBaltacs@users.noreply.github.com>
2022-11-15 10:36:32 +0200

    Add safe area view.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import {
   Image,
   StyleSheet,
   useWindowDimensions,
+  Platform,
+  StatusBar,
+  SafeAreaView,
 } from "react-native";
 import { Searchbar } from "react-native-paper";
 import debounce from "lodash/debounce";
@@ -13,6 +16,7 @@ import PRIVATE_KEY from "./Constants";
 
 const App = () => {
   const [isLoading, setLoading] = useState(true);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
   const [data, setData] = useState([]);
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
@@ -20,7 +24,7 @@ const App = () => {
 
   useEffect(() => {
     setData([]);
-    // resetOffset();
+    console.log("query", query);
   }, [query]);
 
   const WINDOW_PADDING = 10;
@@ -33,10 +37,9 @@ const App = () => {
   };
 
   const loadData = (input, page, offset) => {
+    setIsLoadingMore(true);
     setPage(page + 1);
     setOffset(itemLimit * page);
-    // console.log("offset in loadData =>", offset);
-    // console.log("page in loadData =>", page);
     fetch(
       `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=${PRIVATE_KEY}&limit=${itemLimit}&offset=${offset}`,
       {
@@ -50,68 +53,73 @@ const App = () => {
       .then((response) => response.json())
       .then((json) => {
         if (json?.data.length > 0) setData([...data, ...json?.data]); // Vai šis if jau nenohandlo situāciju, kur data array nav jāpapildina?
+        console.log("data after fetch", ...data);
       })
       .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-    console.log("Run loadData");
+      .finally(
+        () => setLoading(false)
+        // , setIsLoadingMore(false)
+      );
   };
 
   // #TODO: Nepushot Constants failu
   const handleSearch = useCallback(
     debounce((input = "") => {
       loadData(input, 1);
-      console.log("Run debounce");
     }, 300),
     [offset]
   );
+
+  // const LoadMoreGifs = () => {
+  //   return isLoading ? <Text>Loading more GIFs...</Text> : null;
+  // };
 
   useEffect(() => {
     handleSearch(query, [query]);
   }, [query]);
 
+  console.log("data being displayed:", data);
   return (
-    <View style={{ flex: 1, paddingHorizontal: WINDOW_PADDING }}>
-      {isLoading ? (
-        <Text>Loading...</Text>
-      ) : (
-        // #TODO: pielikt stilu
-        <View style={styles.topContainer}>
-          <Text style={styles.title}>Chili GIFs:</Text>
-          <Searchbar
-            placeholder="Type your search here"
-            onChangeText={handleChange}
-            value={query}
-            // value={input}
-            style={styles.searchBar}
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <View style={{ flex: 1, paddingHorizontal: WINDOW_PADDING }}>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <View style={styles.topContainer}>
+            <Text style={styles.title}>Chili GIFs:</Text>
+            <Searchbar
+              placeholder="Type your search here"
+              onChangeText={handleChange}
+              value={query}
+              style={styles.searchBar}
 
-            // #TODO: ieviest onSend. Tagad nekas nenotiek.
-          />
+              // #TODO: ieviest onSend. Tagad nekas nenotiek.
+            />
 
-          <FlatList
-            data={data}
-            keyExtractor={(item) => item.id}
-            numColumns={3}
-            onEndReachedThreshold={0.5}
-            onEndReached={() => {
-              // handleSearch(query);
-              loadData(query, page, offset);
-            }}
-            renderItem={({ item }) => (
-              <Image
-                source={{
-                  uri: item.images.preview_gif.url,
-                }}
-                style={{ width: gifSize, height: gifSize, margin: 2 }}
-                // #TODO: pielikt border top
-              />
-            )}
-          />
-          {/* <View>
-            {isLoadingMoreData ? <Text>Loading more GIFs...</Text> : null}
-          </View> */}
-        </View>
-      )}
-    </View>
+            <FlatList
+              data={data}
+              keyExtractor={(item) => item.id}
+              numColumns={3}
+              onEndReachedThreshold={0.5}
+              onEndReached={() => {
+                loadData(query, page, offset);
+              }}
+              // ListFooterComponent={LoadMoreGifs}
+              renderItem={({ item }) => (
+                <Image
+                  source={{
+                    uri: item.images.preview_gif.url,
+                  }}
+                  style={{ width: gifSize, height: gifSize, margin: 2 }}
+                  // #TODO: pielikt border top
+                />
+              )}
+            />
+            <View>{isLoading ? <Text>Loading more GIFs...</Text> : null}</View>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -122,7 +130,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 14,
-    color: "green",
+    color: "#010101",
     textAlign: "center",
     paddingTop: 30,
     paddingBottom: 10,
@@ -130,6 +138,12 @@ const styles = StyleSheet.create({
   searchBar: {
     height: 40,
     marginBottom: 10,
+  },
+  // AndroidSafeArea view style taken from: https://stackoverflow.com/questions/51289587/how-to-use-safeareaview-for-android-notch-devices
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
 
